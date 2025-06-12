cf2eea4a3da6dec5f5ef0d668d906a794252b424
rsm-lab-code@rsmus.com <rsm-lab-code@rsmus.com>
2025-06-12 21:55:56 +0000

    up

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,11 @@ output "scp_policy_arns" {
     for k, v in aws_organizations_policy.scp_policies : k => v.arn
   }
 }
+
+output "debug_scp_vars" {
+  value = {
+    attach_scp_policies = var.attach_scp_policies
+    scp_target_ou_id = var.scp_target_ou_id
+    management_account_id = var.management_account_id
+  }
+}
